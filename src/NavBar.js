ea54ee10aee5c437a5e7139fbfef8b9b2a3d4fd7
Kalyan Chavali <kalyan.chavali123@gmail.com>
2022-03-16 21:49:18 -0600

    Added more components to the home page

--- src/NavBar.js
+++ src/NavBar.js
@@ -5,15 +5,12 @@ const NavBar = () => {
   return (
     <div>
       <nav className="navbar main-nav navbar-expand-sm navbar-collapse fixed-top navbar-light">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div className="container-fluid">
-          <ul className="navbar-nav">
-            <ListItem linkText="Home" />
+        <div className="container-fluid text-right">
+          <ul className="navbar-nav ms-auto flex-nowrap">
             <ListItem linkText="Experience" />
-            <ListItem linkText="Education" />
             <ListItem linkText="Skills" />
+            <ListItem linkText="Education" />
+            <ListItem linkText="Contact" />
           </ul>
         </div>
       </nav>
--- src/index.js
+++ src/index.js
@@ -2,15 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import NavBar from './NavBar';
+import Section from './Section';
+import ExperienceTab from './ExperienceTab';
 
 const Title = () => {
   return (
     <div>
       <NavBar/>
-      <div className="title container-fluid text-center">
+      <div className="title container-fluid p-5 my-5">
+        <span>Hello! My name is</span>
         <h1>Kalyan Chavali</h1>
-        <p>Software Developer III at Workday</p>
+        <h3>Full Stack Software Engineer</h3>
+        <p>I am a software engineer building high quality software<br/>
+        in Java, JavaScript (React), Spring and other technologies.<br/>
+        At present, I'm working in financial planning part of Workday!</p>
       </div>
+      <Section>
+        <h1>Experience</h1>
+        <ExperienceTab />
+      </Section>
+      <Section>
+        <h1>Skills</h1>
+      </Section>
+      <Section>
+        <h1>Education</h1>
+      </Section>
     </div>
   );
 };
